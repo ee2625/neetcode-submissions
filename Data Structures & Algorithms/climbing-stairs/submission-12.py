c7d1c78e4744fedade2a1d9d00f15f976981b790
ee2625 <ereerc.21@gmail.com>
2026-06-29 19:56:55 -0400

    Add: climbing-stairs - submission-12

---
+++ Data Structures & Algorithms/climbing-stairs/submission-12.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        count = {}
+        def fib(n):
+            if n == 1:
+                return 1
+            if n == 2:
+                return 2
+            if n in count:
+                return count[n]
+            count[n] = fib(n-1) + fib(n-2)
+            return count[n]
+        return fib(n)
