a8d36eec0aa235b5a40da02ee2a246089e0b911f
ee2625 <ereerc.21@gmail.com>
2026-07-11 11:17:17 -0400

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = []
+        seen = {}
+        for val in strs:
+            key = ''.join(sorted(val))
+            if key in seen:
+                ans[seen[key]].append(val)
+            else:
+                
+                seen[key] = len(ans)
+                ans.append([val])
+        return ans
