b42dd90e8e1b0767cea0da5572855a74ddffbb07
ee2625 <ereerc.21@gmail.com>
2026-08-01 20:58:14 -0400

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        group = {}
+        ans = []
+        i = 1
+        for x in range(len(numbers)):
+            if target - numbers[x] in group:
+                return [group[target - numbers[x]],i]
+            else:
+                group[numbers[x]] = i
+                i += 1
