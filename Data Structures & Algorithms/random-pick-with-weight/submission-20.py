197d3bda161ab0fe3538ad045811bfc526783f4c
ee2625 <ereerc.21@gmail.com>
2026-08-08 17:05:06 -0400

    Add: random-pick-with-weight - submission-20

---
+++ Data Structures & Algorithms/random-pick-with-weight/submission-20.py
@@ -0,0 +1,26 @@
+import random
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.runningsum = []
+        self.total = 0
+        for val in w:
+            self.total += val
+            self.runningsum.append(self.total)
+
+    def pickIndex(self) -> int:
+        num = random.randint(1,self.total)
+        left = 0 
+        right = len(self.runningsum) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if num <= self.runningsum[mid]:
+                right = mid -1
+            else:
+                left = mid + 1
+        return left
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
